b42ae821be99bd37751e1f61f878042c4d451447
Saibalaji SM <smsaibalaji@gmail.com>
2026-05-16 13:24:02 +0530

    Initial Commit

---
+++ src/main/java/com/bankingapp/accountoperation/AccountoperationApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapp.accountoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountoperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankingapp/accountoperation/AccountoperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingapp.accountoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountoperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
